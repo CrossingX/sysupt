7a7cfa50d581ea13e4527a5c8af64dd0f50559e5
howardlau <howardlau1999@hotmail.com>
2019-12-24 23:10:55 +0800

    删除二维码

--- include/tjuip_helper.php
+++ include/tjuip_helper.php
@@ -16,7 +16,7 @@ function assert_tjuip()
 
         if (!check_tjuip($nip)) {
             stdhead("没有权限");
-            stdmsg("没有访问权限", "<div align=\"center\">你正在使用校外IP地址访问本站，不允许浏览本页面，请尝试通过IPv6网络访问<hr><font size='3' color='red'>扫码调戏吉祥物(●'◡'●)</font><br><img src='images/qrcode-wx.png' width='30%'/><img src='images/qrcode-qq.png' width='30%'/><img src='images/qrcode-weibo.png' width='30%'/></div>");
+            stdmsg("没有访问权限", "<div align=\"center\">你正在使用校外IP地址访问本站，不允许浏览本页面，请尝试通过IPv6网络访问</div>");
             stdfoot();
             exit;
         }
